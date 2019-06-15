5e94c3ce833246121b2bce04619073bb07f0ed40
Julia Kharlamova <julia88.2010@mail.ru>
2019-06-15 16:10:53 +0300

    finish layout

--- src/js/ui.js
+++ src/js/ui.js
@@ -12,14 +12,17 @@ export class UI {
 			 template += `
 			 <ul class="book">
 			 	<li class="book__img"><img src="${book.volumeInfo.imageLinks.smallThumbnail}"></li>
-				<li class="book__title">${book.volumeInfo.title}</li>
-				<li class="book__author">by ${book.volumeInfo.authors}</li>
-				<li class="book__pages">pages: ${book.volumeInfo.pageCount}</li>
-				<li class="book__published">published ${book.volumeInfo.publishedDate}</li>
+					<li class="book__title">${book.volumeInfo.title}</li>
+					<li class="book__author">by ${book.volumeInfo.authors}</li>
+					<li class="book__pages">${book.volumeInfo.pageCount} pages</li>
+					<li class="book__published">published ${book.volumeInfo.publishedDate}</li>
+					<li class=""><a class="book__download" href="${book.accessInfo.webReaderLink}">read on GooglePlay</a></li>
+					<li><a class="book__download" href="${book.accessInfo.epub.acsTokenLink}">epub</a></li>
+					<li><a class="book__download" href="${book.accessInfo.pdf.acsTokenLink}">pdf</a></li>
 			</ul>
 			`
 		});
 		this.result.innerHTML = template;
-		// console.log(data)
+		console.log(data)
 	}
 }
