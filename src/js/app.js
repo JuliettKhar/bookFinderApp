2dcf922eb9a08450cf16d8374a738ef27e352790
Julia Kharlamova <julia88.2010@mail.ru>
2019-06-16 10:12:13 +0300

    add check for template

--- src/js/app.js
+++ src/js/app.js
@@ -3,16 +3,25 @@ import { UI } from './ui.js';
 const api = new Api();
 const ui = new UI();
 
-function getBooks() {
-	const books = api.getData()
+function getBooks(queryString) {
+	const books = api.getData(queryString)
 		.then( data => ui.displayBooks(data))
 		.catch( err => console.log(err))
 }
 
 
+function onClick(event) {
+	const inputValue = ui.input.value;
+	const queryString = inputValue.split(' ').join('+');
+	getBooks(queryString);
+	event.preventDefault();
+}
 
+function subscribe() {
+	ui.btn.addEventListener('click', onClick);
+}
 
 export function init() {
-	getBooks();
-	console.log(ui)
+		subscribe();
+		
 }
--- src/js/ui.js
+++ src/js/ui.js
@@ -4,25 +4,36 @@ export class UI {
 	}
 	init() {
 		this.result = document.querySelector('#results');
+		this.input = document.querySelector('#bookSearch');
+		this.btn = document.querySelector('.search__button');
 	}
 	displayBooks(data) {
+		console.log(data)
 		const instance = data.response.items;
 		let template = '';
 		instance.forEach( book => {
+			console.log(book.volumeInfo.imageLinks.smallThumbnail)
 			 template += `
 			 <ul class="book">
-			 	<li class="book__img"><img src="${book.volumeInfo.imageLinks.smallThumbnail}"></li>
+			 	<li class="book__img"><img src="${book.volumeInfo.imageLinks.smallThumbnail !== undefined ? book.volumeInfo.imageLinks.smallThumbnail : ' '}" alt="image is not aviliable"></li>
 					<li class="book__title">${book.volumeInfo.title}</li>
-					<li class="book__author">by ${book.volumeInfo.authors}</li>
-					<li class="book__pages">${book.volumeInfo.pageCount} pages</li>
-					<li class="book__published">published ${book.volumeInfo.publishedDate}</li>
-					<li class=""><a class="book__download" href="${book.accessInfo.webReaderLink}">read on GooglePlay</a></li>
+					<li class="book__author">by ${book.volumeInfo.authors !== undefined ? book.volumeInfo.authors : ' - '}</li>
+					<li class="book__pages">${book.volumeInfo.pageCount !== undefined ? book.volumeInfo.pageCount : ' - '} pages</li>
+					<li class="book__published">Published ${book.volumeInfo.publishedDate !== undefined ? book.volumeInfo.publishedDate : ' - '}</li>
+					<li class=""><a class="book__download" href="${book.accessInfo.webReaderLink}" target_blank>read on GooglePlay</a></li>
 					<li><a class="book__download" href="${book.accessInfo.epub.acsTokenLink}">epub</a></li>
 					<li><a class="book__download" href="${book.accessInfo.pdf.acsTokenLink}">pdf</a></li>
 			</ul>
 			`
-		});
+		})
 		this.result.innerHTML = template;
 		console.log(data)
 	}
+
+// 	displayError(message, className) {
+// 			const errorMessage = document.createElement('p');
+// 			errorMessage.className = className;
+// 			errorMessage.appendClild(document.createTextNode(message));
+// 			this.result.appendChild(errorMessage);
+// }
 }
